aa25367bbb33e283361652a7cdad0952e74b4080
Jeremy Dunn <jeremy@viastudio.com>
2015-05-25 14:07:37 -0500

    layout

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,36 +1,30 @@
 <?php namespace App\Http\Controllers;
 
-class WelcomeController extends Controller {
+use Illuminate\Auth\AuthServiceProvider;
+use Illuminate\Support\Facades\App;
 
-	/*
-	|--------------------------------------------------------------------------
-	| Welcome Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller renders the "marketing page" for the application and
-	| is configured to only allow guests. Like most of the other sample
-	| controllers, you are free to modify or remove it as you desire.
-	|
-	*/
+class WelcomeController extends Controller
+{
+    /**
+     * Tumblr Client
+     *
+     * @var \Tumblr\API\Client
+     */
+    protected $tumblrClient;
 
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('guest');
-	}
+    public function __construct(\Tumblr\API\Client $tumblrClient)
+    {
+        $this->tumblrClient = $tumblrClient;
+        $this->middleware('guest');
+    }
 
-	/**
-	 * Show the application welcome screen to the user.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		return view('welcome');
-	}
-
+    /**
+     * Show the application welcome screen to the user.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('welcome');
+    }
 }
