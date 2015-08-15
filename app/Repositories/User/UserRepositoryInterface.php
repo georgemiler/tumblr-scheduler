0d1c63d12bf64dbd241b88fdd71ebf18900a6861
Jeremy Dunn <jeremy@viastudio.com>
2015-08-15 17:39:17 -0500

    cleanup

--- app/Repositories/User/UserRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Repositories\User;
-
-use App\Repositories\RepositoryInterface;
-
-interface UserRepositoryInterface extends RepositoryInterface
-{
-    public function findUserByEmail($email);
-}
